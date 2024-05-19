f8c7f60881cb7a3209aafccd5d0faa598ee10e7e
21127617 <phamhuy1112003@gmail.com>
2024-05-19 13:24:54 +0700

    Initial project

---
+++ MovieEndPoints/MoviesEndPoints.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieTickets.Api.EndPoints;
+
+public static class MoviesEndPoints
+{
+    public static RouteGroupBuilder MapMoviesEndPoints(this WebApplication app)
+    {
+        var group = app.MapGroup("movies");
+
+        group.Map("/", () => { });
+
+        return group;
+    }
+}
