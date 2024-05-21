3f8a44bd2e5212d97f261fc1471b39c03d098225
21127617 <phamhuy1112003@gmail.com>
2024-05-21 19:40:48 +0700

    add CORS

--- Program.cs
+++ Program.cs
@@ -5,12 +5,25 @@ var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("BookingMovieTicketsApp");
 builder.Services.AddSqlServer<BookingMovieTicketsContext>(connString);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder
+            .WithOrigins("http://localhost:5288")
+            .AllowAnyHeader()
+            .WithMethods("GET", "POST", "PATCH", "DELETE")
+            .SetIsOriginAllowed((host) => true)
+            .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
+app.UseCors(); // this line is added
 
 // Mapping
 app.MapMoviesEndPoints();
 app.MapGenresEndPoints();
-
 
 await app.MigrateDbAsync();
 
